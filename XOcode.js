932abfc85fe92f58b08a7e78768114c17295eb73
AlbertHakobyan2 <92681226+AlbertHakobyan2@users.noreply.github.com>
2021-12-14 02:39:27 +0400

    Add files via upload

--- XOcode.js
+++ XOcode.js
@@ -79,25 +79,33 @@ module.exports = tictactoe = {
                             mat[x][y]="O";
                             this.checkers.checkWin(mat);
                             this.printXOmatrix(mat);
-                                 this.startx=0;
+                            this.startx=0;
                             this.InsertX();
-                      this.starto++;
+                            this.starto++;
                             return this.starto, mat; 
                           }  
                         } 
-                  } else if (this.checkers.checkWin(this.xoboard) == true){ 
-                           console.log("           Congratulations! player X Won This Time " + '\n');
-                           this.checkers.playAgain(); 
-                    }else return mat;
+                      } else if (this.checkers.checkWin(this.xoboard) == true){ 
+                        console.log("           Congratulations! player X Won This Time " + '\n');
+                        this.checkers.playAgain(); 
+                      }else return mat;
                   },
                   
                   
-                  checkers: a = { 
+        checkers: a = { 
+                    
                     normalMat: [[],[],[]],
                     coltoRow:  [[],[],[]],
-                    
-                    
-                    rangeCheck: function (elem1, elem2){
+                
+                    tieCondition: function(steps){
+                      if (steps == 10){
+                        console.log("\n" + "TIE" + "\n");
+                        return this.playAgain();
+                      } 
+                    },
+
+
+              rangeCheck: function (elem1, elem2){
                       return ((elem1 > 0 && elem1 < 4) && (elem2 > 0 && elem2 < 4));
               },
               
@@ -123,109 +131,107 @@ module.exports = tictactoe = {
               checkWin: function(mat) { 
                 let win = false;
                 for (let i = 0; i < mat.length; i++){
-                  if (this.rowWin(i, mat) || this.colWin(i, mat) || this.diagcheck(i, mat)){
+                  if (this.rowWin(i, mat) || this.colWin(i, mat) || this.diagWin(mat)){
                     win = true; break;
-                  } else if (!this.rowWin(i, mat) && !this.colWin(i, mat)&& !this.diagcheck(i, mat)){
+                  } else if (!this.rowWin(i, mat) && !this.colWin(i, mat)&& !this.diagWin(i, mat)){
                     win = false;
                   }                                                          
                 } return win; 
               },
               
-              wincheckX: function(str){
-                return ((str === "X")) 
-              },
-              wincheckO: function(str){
-                return str === "O"; 
-              },
+              
+              
+              
+              colWin: function(x, mat){
+                  if (this.colcheck(x, mat)){
+                    return true;
+                  } else return false;
+                },
+                
+                diagWin: function (mat){
+                    let diag1 = [];
+                    let diag2 = [];
+                    for (let row = 0; row < mat.length; row++){   
+                      diag1.push(mat[row][row]);
+                      } 
+                    for (let row = 0; row < mat.length; row++) {
+                      for (let col = 0; col < mat[row].length; col++) {
+                        if (col + row === mat[row].length - 1) {
+                              diag2.push(mat[row][col]);
+                            }
+                      }
+                    } 
+                    return (this.diagXOcheck(diag1) || this.diagXOcheck(diag2));
+                  },
+                             diagXOcheck: function(arr){
+                                     return (arr.every(r => this.wincheckX(r)) || 
+                                     arr.every(r => this.wincheckO(r)));
+                                     },                
+                                     
+              backToNormal: function (mat){                       // During the check processes, the XOboard (here:mat), secretly becomes 
+                  for(let i = 0; i < mat.length; i++){            // a giant matrix that shows us its checked, shy and beautiful 
+                    for (let j = 0; j < mat[i].length; j++){      // self every time. Thus we must have its instances independent from that 
+                      this.normalMat[i][j] = mat[j][i];           //  giant monster. That's why its called BacktoNormal  ](:-])={:}==|==[
+                    }
+                  }
+                  return this.normalMat; 
+                },
 
-              tieCondition: function(steps){
-                if (steps == 10){
-                  console.log("\n" + "TIE" + "\n");
-                  return this.playAgain();
+                      colmatrixbuilder: function(mat){
+                            this.backToNormal(mat);
+                            for(let i = 0; i < this.normalMat.length; i++ ){    //since we are always sure about the nornakMat, 
+                                this.coltoRow[i] =  this.normalMat[i].slice();  //we call the slice function that seperates elements of the array
+                             } 
+                              return this.coltoRow;       // This is the rotated matrix that will get checked by the same rowcheck
+                       },
+              
+              colcheck: function (x, mat){
+                this.colmatrixbuilder(mat);               // executes the 2 functions above and changes the instance of the matrix that gets rotated  
+                return this.rowcheck(x, this.coltoRow);      
+              },
+              
+              
+              rowcheck: function(elements, mat){
+                return (mat[elements].every(r => this.wincheckX(r)) || 
+                        mat[elements].every(r => this.wincheckO(r)));
+                        },
+                          wincheckX: function(sign){
+                                     return sign === "X";
+                           },
+                          wincheckO: function(sign){
+                                     return sign === "O"; 
+                           },
+              
+              rowWin: function(x, mat){
+                let check = false;
+                if ((x > 2)||!this.rowcheck(x, mat)) return check;
+                else if ((x >=0)&&(x <= 2)&&!this.rowcheck(x, mat)){ 
+                  return this.rowWin(x + 1, mat);
+                } else if (this.rowcheck(x, mat)){ 
+                  check = true;   
                 } 
+                return check;
               },
-
+              
               playAgain: function () {
                 let again = reader.question("Game is Over. Do you want to start over? (yes or no): ");
                 let mat;
                 if (again == "yes") {
                   tictactoe.stepCounter = 0;
                   mat = [   
-                     ["_","_","_"],
-                     ["_","_","_"],
-                     ["_","_","_"]
-                    ]; 
+                    ["_","_","_"],
+                    ["_","_","_"],
+                    ["_","_","_"]
+                  ]; 
                   tictactoe.xoboard = mat;
                   return tictactoe.InsertX();
-                  } else if (again == "no"){
-                    return console.log("Game Stoped");
-                  }else{ 
-                    return console.log("Rewrite your choice"), 
+                } else if (again == "no"){
+                  return console.log("Game Stoped");
+                }else{ 
+                  return console.log("Rewrite your choice"), 
                     this.playAgain();
-                    }
-               },
-              
-             
-             backToNormal: function (mat){             
-                 for(let i = 0; i < mat.length; i++){
-                   for (let j = 0; j < mat[i].length; j++){  
-                     this.normalMat[i][j] = mat[j][i]; 
-                    }
                   }
-                return this.normalMat; 
-              },
-              colmatrixbuilder: function(mat){
-                this.backToNormal(mat);
-                 for(let i = 0; i < this.normalMat.length; i++ ){
-                  this.coltoRow[i] =  this.normalMat[i].slice();
-                 } 
-                return this.coltoRow;
-              },
-              
-              colcheck: function (x, mat){
-                this.colmatrixbuilder(mat);
-               return this.rowcheck(x, this.coltoRow);
-              },
-              
-              colWin: function(x, mat){
-                if (this.colcheck(x, mat)){
-                return true;
-                } else return false;
-              },
-              rowcheck: function(elements, mat){
-                  return (mat[elements].every(r => this.wincheckX(r)) || 
-                          mat[elements].every(r => this.wincheckO(r)));
-              },
-              rowWin: function(x, mat){
-                let check = false;
-                if ((x > 2)||!this.rowcheck(x, mat)) return check;
-                   else if ((x >=0)&&(x <= 2)&&!this.rowcheck(x, mat)){ 
-                      return this.rowWin(x + 1, mat);
-                   } else if (this.rowcheck(x, mat)){ 
-                      check = true;   
-                            } 
-                return check;
-              },
-              diagXOcheck: function(elements, arr){
-                return (arr.every(r => this.wincheckX(r)) || 
-                        arr.every(r => this.wincheckO(r)));
-              },                
-              diagcheck: function (x, mat){
-                  let diag1 = [];
-                  let diag2 = [];
-                  for (let row = 0; row < mat.length; row++){   
-                        diag1.push(mat[row][row]);
-                    } 
-                  for (let row = 0; row < mat.length; row++) {
-                    for (let col = 0; col < mat[row].length; col++) {
-                        if (col + row === mat[row].length - 1) {
-                            diag2.push(mat[row][col]);
-                        }
-                    }
-                } 
-                  return (this.diagXOcheck(x, diag1) || this.diagXOcheck(x, diag2));
-               }
-           }
+                }
+         }
 }
-                
-                
+
